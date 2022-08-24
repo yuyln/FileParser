abb31505bd0006e6000b3708cb6ffeb911621e3d
Jose Souza <jc.souza@unesp.br>
2022-08-24 00:49:58 -0300

    remake of parser

--- main.c
+++ main.c
@@ -1,24 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define PARSER_IMPLEMENTATION
 #include "parser.h"
 
-int main(int argc, char **argv)
+int main()
 {
-    int qnt;
-    FILE *f = fopen("test.in", "rb");
-    char *o = ReadFile(f);
-    fclose(f);
-    char **parsed = Parse(o, &qnt);
-    unsigned int A = GetValueUInt("A", parsed, qnt);
-    int B = GetValueInt("B", parsed, qnt);
-    double D = GetValueDouble("D", parsed, qnt);
-    float E = GetValueFloat("E", parsed, qnt);
-    printf("%u\n", A);
-    printf("%d\n", B);
-    printf("%f\n", D);
-    printf("%f\n", E);
-    float T = GetValueFloat("T", parsed, qnt);
-    printf("%f\n", T);
+    StartParse("./test.in");
+    printf("%llu\n", GetValueULLInt("A", 10));
+    printf("%ld\n", GetValueInt("B", 10));
+    printf("%lu\n", GetValueUInt("C", 10));
+    printf("%f\n", GetValueFloat("D"));
+    printf("%f\n", GetValueDouble("E"));
+    EndParse();
     return 0;
 }
